31bd346b005dd7630b12cdb6ea500b0bbdea8980
bemontoya <be.montoya@duocuc.cl>
2026-05-16 19:29:24 -0400

    Se creó base de datos para cliente

---
+++ cliente/src/main/resources/db/migration/V1__crear_tabla_clientes.sql
@@ -0,0 +1,8 @@
+CREATE TABLE clientes (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    telefono VARCHAR(50),
+    fecha_registro DATETIME NOT NULL
+);
