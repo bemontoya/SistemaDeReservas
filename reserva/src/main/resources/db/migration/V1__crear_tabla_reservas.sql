6510c603133afd99f6132dd148516fcbec55c1c9
bemontoya <be.montoya@duocuc.cl>
2026-05-17 00:42:12 -0400

    Se agregó el docker en reserva

--- reserva/src/main/resources/db/migration/V1__crear_tabla_reservas.sql
+++ reserva/src/main/resources/db/migration/V1__crear_tabla_reservas.sql
@@ -2,6 +2,7 @@ CREATE TABLE reservas (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     cliente_id BIGINT NOT NULL,
     mesa_id BIGINT NOT NULL,
-    fecha_reserva DATETIME NOT NULL,
-    estado VARCHAR(50) NOT NULL
+    fecha_reserva TIMESTAMP NOT NULL,
+    cantidad_personas INT NOT NULL,
+    estado VARCHAR(50) NOT NULL DEFAULT 'CONFIRMADA'
 );
