19b773eea40183c9ac6d4babcc99d685fff0dd1a
bemontoya <be.montoya@duocuc.cl>
2026-05-16 23:49:13 -0400

    Se agregó el docker para pagos

--- pagos/src/main/java/Sistema/Reservas/pagos/repository/PagoRepository.java
+++ pagos/src/main/java/Sistema/Reservas/pagos/repository/PagoRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository // Componenete que maneja operaciiones crud (Crear, Leer, Actualizar, Borrar)
 public interface PagoRepository extends JpaRepository<Pago, Long>{
     // Busca si un pedido ya tiene un pago registrado
-    boolean existeByPedidoId(Long pedidoId);
+    boolean existsByPedidoId(Long pedidoId);
 }
