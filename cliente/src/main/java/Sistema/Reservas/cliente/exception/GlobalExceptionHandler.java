2e054c49e1e3e526ea041cc82d4f111bf7d0c8c8
bemontoya <be.montoya@duocuc.cl>
2026-05-18 15:43:30 -0400

    Se agregó el exception para cliente

---
+++ cliente/src/main/java/Sistema/Reservas/cliente/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package Sistema.Reservas.cliente.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errores = new HashMap<>();
+
+        // Buscamos todos los campos que fallaron y su mensaje de error
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errores.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(errores, HttpStatus.BAD_REQUEST);
+    }
+}
