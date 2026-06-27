9ca3acc268526525a96e8c95c291d4b6e55cae07
bemontoya <be.montoya@duocuc.cl>
2026-06-27 18:17:58 -0400

    Cambios en aplication propierties

--- cliente/src/main/resources/db/migration/V1__crear_tabla_clientes.sql
+++ cliente/src/main/resources/db/migration/V1__crear_tabla_clientes.sql
@@ -1,8 +1,8 @@
 CREATE TABLE clientes (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    apellido VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    telefono VARCHAR(50),
-    fecha_registro DATETIME NOT NULL
+                          id BIGSERIAL PRIMARY KEY,
+                          nombre VARCHAR(100) NOT NULL,
+                          apellido VARCHAR(255) NOT NULL,
+                          email VARCHAR(255) NOT NULL UNIQUE,
+                          telefono VARCHAR(50),
+                          fecha_registro TIMESTAMP NOT NULL
 );
